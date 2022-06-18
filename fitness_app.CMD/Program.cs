c91196c315a80f0fa8d457aaa427a28851c38206
Zen <tlp6@yandex.ru>
2022-06-18 18:35:18 +0300

    Создана модель Пользователя

--- fitness_app.CMD/Program.cs
+++ fitness_app.CMD/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace fitness_app.BL
+namespace fitness_app.CMD
 {
-    public class Class1
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+        }
     }
 }
